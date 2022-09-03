62fc40c4cad9561e011fccf42bda41b5f0847a71
ahmarhussain.ah <ahmarhussain.ah@gmail.com>
2022-09-03 23:30:43 +0500

    particles config improved

--- js/main.js
+++ js/main.js
@@ -147,8 +147,6 @@
 		skillsWayPoint();
 	});
 
-	particlesJS.load('particles-js', 'js/particles.json', function() {
-		console.log('callback - particles.js config loaded');
-	});
+	particlesJS.load('particles-js', 'js/particles.json');
 
 }());
